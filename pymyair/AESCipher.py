91f1bf40c2e52b9dcb2c4d332e4d13631b7e076f
Sam Richards <me@smallsam.com>
2017-01-03 23:17:50 +1000

    Updated for python 3

--- pymyair/AESCipher.py
+++ pymyair/AESCipher.py
@@ -2,27 +2,42 @@ from Crypto.Cipher import AES
 from Crypto import Random
 import binascii, base64
 
-BS = 16
-pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
-unpad = lambda s : s[0:-ord(s[-1])]
+#BS = 16
+#pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS) 
+#unpad = lambda s : s[0:-ord(s[-1])]
 
 class AESCipher:
     def __init__( self, key ):
         self.key = key
+        self.bs = 16
 
     def encrypt( self, raw ):
         """
         Returns hex encoded encrypted value!
         """
-        raw = pad(raw)
+        raw = self._pad(raw)
         iv = Random.new().read(AES.block_size);
         cipher = AES.new( self.key, AES.MODE_CBC, iv )
         return ( iv + cipher.encrypt( raw ) ).encode("hex")
 
     def decrypt( self, enc ):
-        iv =  str(bytearray(16))
+        iv =  bytes(16)
         cipher = AES.new(self.key, AES.MODE_CBC, iv )
-        return unpad(cipher.decrypt( enc))
+        return self._unpad(cipher.decrypt( enc)).decode('utf-8')
+
+    @staticmethod
+    def str_to_bytes(data):
+        u_type = type(b''.decode('utf8'))
+        if isinstance(data, u_type):
+            return data.encode('utf8')
+        return data
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * AESCipher.str_to_bytes(chr(self.bs - len(s) % self.bs))
+
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
 
 def swap(s, i, j):
     return ''.join((s[:i], s[j], s[i+1:j], s[i], s[j+1:]))
