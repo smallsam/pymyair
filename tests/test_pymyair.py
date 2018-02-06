15e4da6c375399669acb26d17e072dd5a8765171
Sam Richards <me@smallsam.com>
2018-02-07 06:28:19 +1000

    Updating tests and fixing build

--- tests/test_pymyair.py
+++ tests/test_pymyair.py
@@ -30,9 +30,9 @@ def test_content(response):
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'pymyair.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
+    result = runner.invoke(cli.cli)
+    assert result.exit_code == 2
+    assert 'Usage: ' in result.output
+    help_result = runner.invoke(cli.cli, ['--help'])
     assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+    assert 'Show this message and exit.' in help_result.output
